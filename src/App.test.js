ef8b10b1fd3f309190a209ee5ed85b479b0c9566
dubey-puneet <kiran-ankani>
2021-07-23 19:40:04 +0530

    ddD

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from './App'; 
 
 test('renders My really cool blog site logo', () => {
   render(<App />);
